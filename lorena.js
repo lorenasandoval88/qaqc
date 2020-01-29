877e0bebfd96ec48e4e8fbafe4d7640eb6619546
lorenasandoval88 <43822820+lorenasandoval88@users.noreply.github.com>
2020-01-29 18:01:39 -0500

    lore

--- lorena.js
+++ lorena.js
@@ -1,6 +1,21 @@
-console.log(`lorena.js ran at ${Date()}`)
+function getColumn(table_id, col) {
+    var tab = document.getElementById(table_id);
+    var n = tab.rows.length;
+    var i, s = null, tr, td;
 
-runQAQC=function(data){
-    console.log(`lorena.js runQAQC function ran at ${Date()}`)
-    return '<p style="color:green">Ready for <a href="https://github.com/episphere/qaqc/blob/master/lorena.js" target="_blank">your QAQC code</a>, Lorena :-)</p>'
-}
+    // First check that col is not less then 0
+    if (col < 0) {
+        return null;
+    }
+
+    for (i = 0; i < n; i++) {
+        tr = tab.rows[i];
+        if (tr.cells.length > col) { // Check that cell exists before you try
+            td = tr.cells[col];      // to access it.
+            s += ' ' + td.innerText;
+        } // Here you could say else { return null; } if you want it to fail
+          // when requested column is out of bounds. It depends.
+        }
+      }
+    let column=`<p>column ${getColumn(qaqc.data)}</p>`
+    return txt
